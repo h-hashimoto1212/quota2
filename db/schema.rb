bb1bbf7d4aec51207350bab8d3c478468422ded0
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2020-01-29 14:06:58 +0900

    Create schema.rb

---
+++ db/schema.rb
@@ -0,0 +1,63 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_01_27_124548) do
+
+  create_table "authors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "documents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.integer "date"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_documents_on_author_id"
+  end
+
+  create_table "pictures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image"
+    t.string "imageable_type"
+    t.bigint "imageable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["imageable_type", "imageable_id"], name: "index_pictures_on_imageable_type_and_imageable_id"
+  end
+
+  create_table "quotas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.boolean "selfquote"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "quota_id"
+    t.bigint "author_id"
+    t.bigint "document_id"
+    t.index ["author_id"], name: "index_quotes_on_author_id"
+    t.index ["document_id"], name: "index_quotes_on_document_id"
+    t.index ["quota_id"], name: "index_quotes_on_quota_id"
+  end
+
+  add_foreign_key "documents", "authors"
+  add_foreign_key "quotes", "authors"
+  add_foreign_key "quotes", "documents"
+  add_foreign_key "quotes", "quotas"
+end
