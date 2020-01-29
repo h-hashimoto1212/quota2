1df8186578aed4d1e183b154369bb70ef6adf3b6
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2020-01-29 15:50:19 +0900

    authors documents made optional

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,4 @@
 class Document < ApplicationRecord
   has_many :pictures, as: :imageable
+  belongs_to :authors, optional: true
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,6 @@
 class Quote < ApplicationRecord
   belongs_to :quota
-  belongs_to :document
-  belongs_to :author
+  belongs_to :document, optional: true
+  belongs_to :author, optional: true
   has_many :pictures, as: :imageable
 end
